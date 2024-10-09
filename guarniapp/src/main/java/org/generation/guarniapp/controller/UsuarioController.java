f7474cf1d12f81ff0d800dd8fd183ebf42c1ed04
Alberto Herrera Nieto <alberto.herrera.dev@gmail.com>
2024-10-09 15:06:59 -0600

    Se creo la clase usuarioController

---
+++ guarniapp/src/main/java/org/generation/guarniapp/controller/UsuarioController.java
@@ -0,0 +1,53 @@
+package org.generation.guarniapp.controller;
+
+import java.util.ArrayList;
+
+import org.generation.guarniapp.model.Usuario;
+import org.generation.guarniapp.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.generation.guarniapp.dto.ChangePassword;
+
+@RequestMapping(path="/api/usuarios/")
+@RestController
+public class UsuarioController {
+	private final UsuarioService usuarioService;
+	
+	@Autowired
+	public UsuarioController(UsuarioService usuarioService) {
+		this.usuarioService=usuarioService;
+	}
+	
+	@GetMapping
+	public ArrayList<Usuario> getUsuario(){
+		return usuarioService.getAllUusuario();
+	}
+	
+	@GetMapping(path="{userId}")
+	public Usuario getUsuario(@PathVariable("userId") Long Id) {
+		return usuarioService.getUsuario(Id);
+	}//getUsuario
+	
+	@PostMapping
+	public Usuario addUsuarios(@RequestBody Usuario usuario) {
+		return usuarioService.addUsuario(usuario);
+	}//addUsuario
+	
+	@DeleteMapping(path="{userId}")
+	public Usuario deleteUsuario(@PathVariable("userId") Long Id) {
+		return usuarioService.deleteUsuario(Id);
+	}//deletUsuario
+	
+	@PutMapping(path="{userId}")
+	public Usuario updateUsuario(@RequestBody ChangePassword changePassword,@PathVariable("userId") Long userId) {
+		return usuarioService.updateUsuario(userId,changePassword);
+	}//updateUsuario
+}
