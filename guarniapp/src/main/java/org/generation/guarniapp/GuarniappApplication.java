6bc45d00c81904c58b457835b121e03e9edd6735
Francisco <takincillo@hotmail.com>
2024-10-09 11:14:28 -0600

    se agrega Backend gradle para guarniapp

---
+++ guarniapp/src/main/java/org/generation/guarniapp/GuarniappApplication.java
@@ -0,0 +1,13 @@
+package org.generation.guarniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuarniappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuarniappApplication.class, args);
+	}
+
+}
---
+++ guarniapp/src/test/java/org/generation/guarniapp/GuarniappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.guarniapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuarniappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
