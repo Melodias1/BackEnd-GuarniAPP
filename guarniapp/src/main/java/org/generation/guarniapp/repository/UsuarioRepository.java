87ccbbf9616b4fc98bd467b478af124575164197
Alberto Herrera Nieto <alberto.herrera.dev@gmail.com>
2024-10-11 17:59:05 -0600

    Se creo la interface usuarioRepository

---
+++ guarniapp/src/main/java/org/generation/guarniapp/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package org.generation.guarniapp.repository;
+
+import java.util.Optional;
+
+import org.generation.guarniapp.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	Optional<Usuario> findByEmail(String email);
+}
