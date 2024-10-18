ae0d20d6972b503bd21bf9fdf61d41f6a3424fff
Alberto Herrera Nieto <alberto.herrera.dev@gmail.com>
2024-10-17 19:09:06 -0600

    Se creo el controlador para login

---
+++ guarniapp/src/main/java/org/generation/guarniapp/controller/LoginController.java
@@ -0,0 +1,53 @@
+package org.generation.guarniapp.controller;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+
+import org.generation.guarniapp.config.JwtFilter;
+import org.generation.guarniapp.dto.Login;
+import org.generation.guarniapp.dto.Token;
+import org.generation.guarniapp.model.Usuario;
+import org.generation.guarniapp.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController
+@RequestMapping(path="/api/login/")
+public class LoginController {
+	private final UsuarioService usuarioService;
+
+	public LoginController(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Login> loginUser(@RequestBody Usuario usuario) throws ServletException {
+		Usuario user = usuarioService.validateUser(usuario);
+		if (user!=null) {
+			System.out.println("Usuario valido" + usuario.getEmail());
+			Token token = new Token(generateToken(usuario.getEmail()));
+			return ResponseEntity.ok(new Login(token, usuario));
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+	}
+	
+	private String generateToken(String email) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.HOUR, 12);
+		return Jwts.builder().setSubject(email).claim("role", "user")
+				.setIssuedAt(new Date()).setExpiration(calendar.getTime())
+				.signWith(SignatureAlgorithm.HS256, JwtFilter.secret).compact();
+		
+	}
+	
+
+}
